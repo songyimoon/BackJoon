02b28a6d446a3bb181c0ee1bfb25418d1125e7cd
songyimoon <songyimoon@gmail.com>
2021-08-16 00:51:10 +0900

    202108160051

---
+++ Algorithm/src/test/Q2741.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q2741 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= T; i++) {
+			bw.write(i + "\n");
+		}
+		bw.flush();
+	}
+}
