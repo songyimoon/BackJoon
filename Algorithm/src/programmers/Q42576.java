649af4609f7b9dfb3ecf35c0f3aa4a93bfe3e8f2
songyimoon <songyimoon@gmail.com>
2021-08-23 00:34:42 +0900

    11

---
+++ Algorithm/src/programmers/Q42576.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.*;
+
+public class Q42576 {
+	public String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		int i;
+		for (i = 0; i < completion.length; i++) {
+			if (!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		return participant[i];
+	}
+}
